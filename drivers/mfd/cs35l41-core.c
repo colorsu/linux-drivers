25307900a9d4e27113ceb94605294046138dbdee
David Rhodes <david.rhodes@cirrus.com>
2017-12-13 12:00:44 -0600

    mfd: cs35l41: Move reset to MFD probe
    * Reset GPIO operation moved from cs35l41-codec
      to MFD probe
    
    * Previously, reset would occur after cs35l41_cal
      and cs35l41_bd had probed and read Dev ID
    
    Change-Id: Ib5d98040923e9846b5c681c840eedde079276a5d
    Signed-off-by: David Rhodes <david.rhodes@cirrus.com>

--- drivers/mfd/cs35l41-core.c
+++ drivers/mfd/cs35l41-core.c
@@ -14,6 +14,9 @@
 #include <linux/gpio.h>
 #include <linux/mfd/core.h>
 #include <linux/module.h>
+#include <linux/regulator/consumer.h>
+#include <linux/gpio/consumer.h>
+#include <linux/delay.h>
 #include <linux/platform_device.h>
 
 #include <sound/cs35l41.h>
@@ -23,9 +26,14 @@ static const struct mfd_cell cs35l41_devs[] = {
 	{ .name = "cs35l41-codec", },
 };
 
+static const char * const cs35l41_supplies[] = {
+	"VA",
+	"VP",
+};
+
 int cs35l41_dev_init(struct cs35l41_data *cs35l41)
 {
-	int ret;
+	int ret, i;
 
 	dev_set_drvdata(cs35l41->dev, cs35l41);
 	dev_info(cs35l41->dev, "Prince MFD core probe\n");
@@ -34,19 +42,77 @@ int cs35l41_dev_init(struct cs35l41_data *cs35l41)
 		memcpy(&cs35l41->pdata, dev_get_platdata(cs35l41->dev),
 		       sizeof(cs35l41->pdata));
 
+	cs35l41->mfd_class = class_create(THIS_MODULE,
+				CS35L41_MFD_SYSFS_CLASS_NAME);
+	if (IS_ERR(cs35l41->mfd_class)) {
+		ret = PTR_ERR(cs35l41->mfd_class);
+		dev_err(cs35l41->dev, "err class create\n");
+		return -EINVAL;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(cs35l41_supplies); i++)
+		cs35l41->supplies[i].supply = cs35l41_supplies[i];
+
+	cs35l41->num_supplies = ARRAY_SIZE(cs35l41_supplies);
+
+	ret = devm_regulator_bulk_get(cs35l41->dev, cs35l41->num_supplies,
+					cs35l41->supplies);
+	if (ret != 0) {
+		dev_err(cs35l41->dev,
+			"Failed to request core supplies: %d\n",
+			ret);
+		return ret;
+	}
+
+	ret = regulator_bulk_enable(cs35l41->num_supplies, cs35l41->supplies);
+	if (ret != 0) {
+		dev_err(cs35l41->dev,
+			"Failed to enable core supplies: %d\n", ret);
+		return ret;
+	}
+
+	/* returning NULL can be an option if in stereo mode */
+	cs35l41->reset_gpio = devm_gpiod_get_optional(cs35l41->dev, "reset",
+							GPIOD_OUT_LOW);
+	if (IS_ERR(cs35l41->reset_gpio)) {
+		ret = PTR_ERR(cs35l41->reset_gpio);
+		cs35l41->reset_gpio = NULL;
+		if (ret == -EBUSY) {
+			dev_info(cs35l41->dev,
+				 "Reset line busy, assuming shared reset\n");
+		} else {
+			dev_err(cs35l41->dev,
+				"Failed to get reset GPIO: %d\n", ret);
+			goto err;
+		}
+	}
+
+	if (cs35l41->reset_gpio) {
+		usleep_range(1000, 1100);
+		gpiod_set_value_cansleep(cs35l41->reset_gpio, 1);
+	}
+
+	usleep_range(2000, 2100);
+
 	ret = mfd_add_devices(cs35l41->dev, PLATFORM_DEVID_NONE, cs35l41_devs,
 				ARRAY_SIZE(cs35l41_devs),
 				NULL, 0, NULL);
 	if (ret) {
 		dev_err(cs35l41->dev, "Failed to add subdevices: %d\n", ret);
-		return -EINVAL;
+		ret = -EINVAL;
+		goto err;
 	}
 
 	return 0;
+
+err:
+	regulator_bulk_disable(cs35l41->num_supplies, cs35l41->supplies);
+	return ret;
 }
 
 int cs35l41_dev_exit(struct cs35l41_data *cs35l41)
 {
 	mfd_remove_devices(cs35l41->dev);
+	regulator_bulk_disable(cs35l41->num_supplies, cs35l41->supplies);
 	return 0;
 }
--- include/linux/mfd/cs35l41/core.h
+++ include/linux/mfd/cs35l41/core.h
@@ -14,11 +14,16 @@
 #define CS35L41_MFD_CORE_H
 
 #include <linux/regmap.h>
+#include <linux/regulator/consumer.h>
 
 struct cs35l41_data {
 	struct cs35l41_platform_data *pdata;
 	struct device *dev;
 	struct regmap *regmap;
+	struct class *mfd_class;
+	struct regulator_bulk_data supplies[2];
+	struct gpio_desc *reset_gpio;
+	int num_supplies;
 	int irq;
 };
 
