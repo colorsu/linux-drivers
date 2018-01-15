6c6a346214e7df87148c825f472eaec4e959abaa
David Rhodes <david.rhodes@cirrus.com>
2018-01-15 12:07:32 -0600

    ASoC: cs35l41: Configure IRQ GPIO before IRQ register
    * Confiigure the IRQ GPIO before the IRQ handler is
      requested to prevent spurious interrupts while the
      pin is in a high-impedance state
    
    Change-Id: I91e00ed6a82816126f8ef2e0ded5fdf9e3f6b403
    Signed-off-by: David Rhodes <david.rhodes@cirrus.com>

--- sound/soc/codecs/cs35l41.c
+++ sound/soc/codecs/cs35l41.c
@@ -949,8 +949,6 @@ static int cs35l41_codec_probe(struct snd_soc_codec *codec)
 {
 	struct cs35l41_private *cs35l41 = snd_soc_codec_get_drvdata(codec);
 	struct classh_cfg *classh = &cs35l41->pdata.classh_config;
-	struct cs35l41_irq_cfg *irq_gpio_cfg1 = &cs35l41->pdata.irq_config1;
-	struct cs35l41_irq_cfg *irq_gpio_cfg2 = &cs35l41->pdata.irq_config2;
 
 	/* Set Platform Data */
 	if (cs35l41->pdata.sclk_frc)
@@ -1063,6 +1061,17 @@ static int cs35l41_codec_probe(struct snd_soc_codec *codec)
 					CS35L41_CH_WKFET_THLD_SHIFT);
 	}
 
+	wm_adsp2_codec_probe(&cs35l41->dsp, codec, false);
+
+	return 0;
+}
+
+static int cs35l41_irq_gpio_config(struct cs35l41_private *cs35l41)
+{
+	struct cs35l41_irq_cfg *irq_gpio_cfg1 = &cs35l41->pdata.irq_config1;
+	struct cs35l41_irq_cfg *irq_gpio_cfg2 = &cs35l41->pdata.irq_config2;
+	int irq_pol = 0;
+
 	if (irq_gpio_cfg1->is_present) {
 		if (irq_gpio_cfg1->irq_pol_inv)
 			regmap_update_bits(cs35l41->regmap,
@@ -1102,9 +1111,12 @@ static int cs35l41_codec_probe(struct snd_soc_codec *codec)
 
 	}
 
-	wm_adsp2_codec_probe(&cs35l41->dsp, codec, false);
+	if (irq_gpio_cfg2->irq_src_sel == CS35L41_GPIO_CTRL_ACTV_LO)
+		irq_pol = IRQF_TRIGGER_LOW;
+	else if (irq_gpio_cfg2->irq_src_sel == CS35L41_GPIO_CTRL_ACTV_HI)
+		irq_pol = IRQF_TRIGGER_HIGH;
 
-	return 0;
+	return irq_pol;
 }
 
 static int cs35l41_codec_remove(struct snd_soc_codec *codec)
@@ -1408,6 +1420,7 @@ static int cs35l41_probe(struct platform_device *pdev)
 	int ret;
 	u32 regid, reg_revid, i, mtl_revid, int_status, chipid_match;
 	int timeout = 100;
+	int irq_pol = IRQF_TRIGGER_HIGH;
 
 	cs35l41 = devm_kzalloc(&pdev->dev, sizeof(struct cs35l41_private), GFP_KERNEL);
 	if (!cs35l41)
@@ -1462,11 +1475,13 @@ static int cs35l41_probe(struct platform_device *pdev)
 		goto err;
 	}
 
+	irq_pol = cs35l41_irq_gpio_config(cs35l41);
+
 	init_completion(&cs35l41->global_pdn_done);
 	init_completion(&cs35l41->global_pup_done);
 
 	ret = devm_request_threaded_irq(cs35l41->dev, cs35l41->irq, NULL,
-				cs35l41_irq, IRQF_ONESHOT | IRQF_TRIGGER_LOW,
+				cs35l41_irq, IRQF_ONESHOT | irq_pol,
 				"cs35l41", cs35l41);
 
 	/* CS35L41 needs INT for PDN_DONE */
